5db709383d553cd734e414ae2efa74818a25589b
ZgBinRs <13093982+zgbinrs@user.noreply.gitee.com>
2023-07-17 13:52:23 +0800

    实现功能1.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'course-table',
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   }
 })
